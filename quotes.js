f8b912b393605bf35f678ad9e4a65fafe4304016
ibrahimkewan0 <ibrahimkewan@outlook.de>
2024-07-16 11:43:36 +0200

    testing test

---
+++ quotes.js
@@ -0,0 +1,30 @@
+const localQuotes = [
+        {
+            "quote": "Entschuldigung, was wollten Sie mich fragen? Ach ja, dieser blöde Plastikbehälter, den ich dich bitten wollte, zu kaufen. Weißt du, Flusssäure frisst sich nicht durch Plastik, aber sie löst Metall, Stein, Glas und Keramik auf. Das war's also.",
+            "Autor": "Walter White"
+        },
+        {
+            "quote": "Alles klar, ich habe jetzt das sprechende Kissen... Okay?",
+            "author": "Walter White"
+        },
+        {
+            "quote": "Wir haben versucht, dich zu vergiften. Wir haben versucht, dich zu vergiften, weil du ein wahnsinniges, degeneriertes Stück Dreck bist und den Tod verdienst.",
+            "Autor": "Walter White"
+        },
+        {
+            "quote": "Na gut, ich sag euch was. Ihr zieht es euch beide sofort aus dem Hintern, oder ich schnappe mir eine Taschenlampe und eine Zange und gehe auf Erkundungstour.",
+            "author": "Jesse Pinkman"
+        },
+        {
+            "quote": "Ich glaube nicht, dass Angst ein wirksamer Motivator ist. Ich will Investitionen. Im Moment bin ich einfach an einem Zeitrahmen interessiert.",
+            "author": "Gustavo Fring"
+        },
+        {
+            "quote": "Wenn man Kinder hat, hat man immer Familie. Sie werden immer deine Priorität sein, deine Verantwortung. Und ein Mann, ein Mann sorgt vor. Und er tut es auch dann, wenn er nicht geschätzt oder respektiert oder sogar geliebt wird. Er steht einfach auf und tut es. Weil er ein Mann ist.",
+            "autor": "Gustavo Fring"
+        },
+        {
+            "quote": "Hey, sag deinem Deppen-Schwager, er soll auf das Licht zugehen.",
+            "author": "Jesse Pinkman"
+        }
+    ];
--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-
-let apiQuotes = [];
-// Get Qoute From API
-async function getQuotes(){
-    //const ApiUtl = await fetch('quotes.json');
-    const ApiUtl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    try{
-        const response = await fetch(ApiUtl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
-    }catch (error){
-        console.log("error Qoute --> " + error);
-    }
+// Show a new quote
+function newQuote(){
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
 }
 
-
-// On Load
-getQuotes();
+// On load
+newQuote();
