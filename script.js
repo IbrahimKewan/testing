731b2299f228c04e87f90a298d8ab7eee1214163
ibrahimkewan0 <ibrahimkewan@outlook.de>
2024-07-16 11:15:10 +0200

    testing test

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+
+let apiQuotes = [];
+// Get Qoute From API
+async function getQuotes(){
+    //const ApiUtl = await fetch('quotes.json');
+    const ApiUtl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+        const response = await fetch(ApiUtl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes[12]);
+    }catch (error){
+        console.log("error Qoute --> " + error);
+    }
+}
+
+
+// On Load
+getQuotes();
